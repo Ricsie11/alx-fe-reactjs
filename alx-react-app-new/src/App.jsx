0d4aa398e65e044b6dff5cc8ce790e96a7c26e95
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-11-06 12:03:47 +0100

    Used useState to design a counter game

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
@@ -12,6 +13,7 @@ function App() {
       <MainContent />
       <WelcomeMessage />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       <Footer />
     </>
   );
