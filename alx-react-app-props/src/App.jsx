86a194fae03ac393b878522735b2a8335b641949
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-11-06 14:35:56 +0100

    Refactor components to use UserContext
    - Removed userData props from ProfilePage, UserDetails, and UserInfo
    - Wrapped components inside UserContext.Provider in App.jsx
    - Updated UserDetails and UserInfo to consume context using useContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,20 +1,30 @@
 import "./App.css";
-import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import UserProfile from "./components/UserProfile";
 import WelcomeMessage from "./components/WelcomeMessage";
+import UserContext from "./UserContext";
+import UserDetails from "./UserDetails";
+import UserInfo from "./UserInfo";
+import ProfilePage from "./ProfilePage"
 
 function App() {
+  const userData = {
+    name: "John Doe",
+    email: "john@example.com",
+  };
   return (
     <>
       <Header />
       <MainContent />
       <WelcomeMessage />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Counter />
-
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+        <UserDetails />
+        <UserInfo />
+      </UserContext.Provider>
       <Footer />
     </>
   );
