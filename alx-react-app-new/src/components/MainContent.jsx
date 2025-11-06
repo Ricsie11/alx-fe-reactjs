d8d92fc249b15c17e6deef6ad94d3dadd00bcc0e
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-11-06 11:41:48 +0100

    Duplicated my file and styled it

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return ( 
+        <main style={{color: 'green', border: '0.5px solid red', padding: '2px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+     );
+}
+
+export default MainContent;
