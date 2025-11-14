bd497e49a26af696f99b77d7ffd66f6c195a0876
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-11-14 17:28:07 +0100

    fix: Correct import path for FavoritesList component and restore its implementation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,7 +3,7 @@ import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FavoritesList from "./components/FavoritesList ";
+import FavoritesList from "./components/FavoritesList";
 import RecommendationsList from "./components/RecommendationsList";
 
 function App() {
