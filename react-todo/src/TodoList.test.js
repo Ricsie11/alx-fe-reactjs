7d62c6cddd8d5b386efcd5c386f2f11150cc439c
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-12-14 22:58:05 +0100

    test: refactor TodoList tests to use TestTodoList wrapper component

--- react-todo/src/TodoList.test.js
+++ react-todo/src/TodoList.test.js
@@ -1,30 +1,31 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import TodoList from "./components/TodoList";
+import TodoList from "../components/TodoList";
+
+function TestTodoList() {
+  return <TodoList />;
+}
 
 describe("TodoList Component", () => {
   test("renders initial todos", () => {
-    render(<TodoList />);
-
+    render(<TestTodoList />);
     expect(screen.getByText("Learn React")).toBeInTheDocument();
     expect(screen.getByText("Write Tests")).toBeInTheDocument();
   });
 
   test("adds a new todo", () => {
-    render(<TodoList />);
+    render(<TestTodoList />);
+    const input = screen.getByPlaceholderText("Add todo");
+    const addButton = screen.getByText("Add");
 
-    fireEvent.change(screen.getByPlaceholderText("Add todo"), {
-      target: { value: "New Todo" },
-    });
-
-    fireEvent.click(screen.getByText("Add"));
+    fireEvent.change(input, { target: { value: "New Todo" } });
+    fireEvent.click(addButton);
 
     expect(screen.getByText("New Todo")).toBeInTheDocument();
   });
 
   test("toggles a todo completion", () => {
-    render(<TodoList />);
-
+    render(<TestTodoList />);
     const todo = screen.getByText("Learn React");
 
     fireEvent.click(todo);
@@ -33,11 +34,12 @@ describe("TodoList Component", () => {
   });
 
   test("deletes a todo", () => {
-    render(<TodoList />);
-
+    render(<TestTodoList />);
     const deleteButtons = screen.getAllByText("Delete");
+
     fireEvent.click(deleteButtons[0]);
 
     expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
   });
 });
+
